0635e309afb35d5d231e92ab51b150ef66e020d8
Ansh Mehta <ansh.mehta.y24@gmail.com>
2024-01-31 14:08:58 +0530

    classes In JS

---
+++ JS Day 26 to 30/ClassesINJS.js
@@ -0,0 +1,45 @@
+//ES6
+
+// class User{
+//     constructor(username, email, password){
+//         this.username = username;
+//         this.email = email;
+//         this.password = password;
+
+//     }
+
+//     encryptPassword(){
+//         return `${this.password}abc`;
+//     }
+//     changeUsername(){
+//         return `${this.username.toUpperCase()}`
+//     }
+// }
+
+
+// const code = new User("Ansh", "code@247", "2232")
+
+// console.log(code.encryptPassword());
+// console.log(code.changeUsername());
+
+
+//behind the scene
+
+function User(username, email, password){
+    this.username = username;
+    this.email = email;
+    this.password = password;
+}
+
+User.prototype.encryptPassword = function(){
+    return `${this.password}abc`;
+}
+
+User.prototype.changeUsername = function(){
+    return `${this.username.toUpperCase()}`
+}
+
+const tea = new User("tea", "tea@gmail.com", "123")
+
+console.log(tea.encryptPassword());
+console.log(tea.changeUsername());
